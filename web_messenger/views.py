eb29e03aa727f4b5a68441cff1fb62cb8d3deac2
Chandni-Hothi <34437574+Chandni-Hothi@users.noreply.github.com>
2017-12-22 11:56:38 +0530

    login and register view

--- web_messenger/views.py
+++ web_messenger/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def login(request):
+
+    return render(request,'login1.html',{})
+
+def validation(request):
+    unm1=request.POST['user']
+    pass1=request.POST['password']
+
+    return HttpResponse(unm1)
+    #excep
+
+def registration(request):
+    return render(request,'registration.html',{})
+
+def register(request):
+
+    fnm1=request.POST['fnm']
+    lnm1=request.POST['lnm']
+    email1=request.POST['email']
+    password1=request.POST['pass']
+
+    return HttpResponse("hello")
+
+def test(request):
+    return render(request,'test.html',{})
+
+
 
 # Create your views here.
