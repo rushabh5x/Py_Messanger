ddc7a221831384d0edde5b6a3f7df94748762aa5
Chandni-Hothi <34437574+Chandni-Hothi@users.noreply.github.com>
2018-01-08 10:52:56 +0530

    half way reload

--- web_messenger/urls.py
+++ web_messenger/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^register',views.register,name='register'),
     url(r'^test',views.test,name='test'),
     url(r'^abcd',views.abcd,name='abcd'),
-    url(r'^sendmessage',views.sendmessage,name='sendmessgae')
+    url(r'^sendmessage',views.sendmessage,name='sendmessgae'),
+    url(r'^autorefresh',views.autorefresh,name='autorefresh')
 ]
